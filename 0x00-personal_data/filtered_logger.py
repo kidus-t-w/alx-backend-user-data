968959cdbdeb94f1cb39ba37321dcc3bb3ddb8b6
kidus-t-w <kidusk69@gmail.com>
2024-05-16 22:47:14 +0300

    Added Format method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -26,33 +26,34 @@ def filter_datum(fields: List[str], redaction: str,
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]) -> None:
-        """Initialize the RedactingFormatter with the specified fields.
+    def __init__(self, fields):
+        """
+        Initializes the RedactingFormatter instance.
 
         Args:
-            fields (List[str]): The list of fields to filter in log messages.
+            fields: The list of fields to be redacted.
         """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Format the log record by filtering sensitive fields
+        """
+        A function that formats the log record message
+        by redacting sensitive fields.
 
         Args:
             record (logging.LogRecord): The log record to be formatted.
 
         Returns:
-            str: The formatted log message with sensitive fields filtered.
+            str: The formatted log message.
         """
-        # Get the original log message
-        original_message = super(RedactingFormatter, self).format(record)
-        # Filter the message
-        filtered_message = filter_datum(
-            self.fields, self.REDACTION, original_message, self.SEPARATOR)
-        return filtered_message
+        record.msg = filter_datum(
+            self.fields, RedactingFormatter.REDACTION,
+            record.getMessage(), RedactingFormatter.SEPARATOR)
+        return super().format(record)
