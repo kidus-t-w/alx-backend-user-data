0fd55fda732ec1ca4670e875fff01be80009aa28
kidus-t-w <kidusk69@gmail.com>
2024-05-16 10:31:34 +0300

    Made code strongly typed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,8 +4,9 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """Returns the log message obfuscated with strongly typed arguments and return type."""
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Returns the log message obfuscated"""
     for field in fields:
         message = re.sub(
             f"{field}=[^{separator}]*", f"{field}={redaction}", message)
