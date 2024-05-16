9a87532ddd225dfe0c43b5c4ba74371e0b41ee8e
kidus-t-w <kidusk69@gmail.com>
2024-05-16 10:06:41 +0300

    Added filter_datum function

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,8 @@
+import re
+"""Returns the log message obfuscated"""
+
+
+def filter_datum(fields, redaction, message, separator):
+    for field in fields:
+        message = re.sub(f"{field}=[^;]*", f"{field}={redaction}", message)
+    return message
