309176e94318a2114607052e030920ac6893e16b
kidus-t-w <kidusk69@gmail.com>
2024-05-17 15:31:09 +0300

    Added doc string

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
+"""Encodes password using hash table"""
 import bcrypt
 
+
 def hash_password(password):
+    """
+    A function to hash a password using bcrypt.
+
+    Parameters:
+        password (str): The password to be hashed.
+
+    Returns:
+        bytes: The hashed password.
+    """
     # Generate a salt
     salt = bcrypt.gensalt()
     # Hash the password with the generated salt
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+    return hashed_password.decode('utf-8')
