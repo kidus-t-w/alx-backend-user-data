40a23a1d97a5a69917040f5f8ce0b763faa19957
kidus-t-w <kidusk69@gmail.com>
2024-05-17 15:35:03 +0300

    Added password validator

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,7 +3,7 @@
 import bcrypt
 
 
-def hash_password(password):
+def hash_password(password: str) -> bytes:
     """ Returns a salted, hashed password, which is a byte string """
     encoded = password.encode()
     hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
