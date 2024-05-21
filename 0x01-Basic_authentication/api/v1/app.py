fffb5236eb48d22f8a1e045dbeaf07d28e6640dd
kidus-t-w <kidusk69@gmail.com>
2024-05-21 17:49:44 +0300

    Modified Authentication method

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -26,7 +26,7 @@ def before_request():
     excluded_path = ['/api/v1/status/',
                      '/api/v1/unauthorized/',
                      '/api/v1/forbidden/']
-    if auth is None or auth.require_auth(request.path, excluded_path):
+    if auth and auth.require_auth(request.path, excluded_path):
         if auth.authorization_header(request) is None:
             abort(401)
         if auth.current_user(request) is None:
