b4e70d9f7a904d557c4c2218f5cdb252f04268ec
kidus-t-w <kidusk69@gmail.com>
2024-05-21 19:05:19 +0300

    added extract_base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,13 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic Authentication"""
-    ...
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        if type(authorization_header) == str:
+            header = authorization_header.split()
+            if len(header) == 2:
+                basic = header[0] + " "
+                authorization = header[1]
+                if basic == "Basic ":
+                    return authorization
+        return None
