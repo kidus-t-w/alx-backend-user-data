35c9cc68a582a53f6d36ed3eee0e75f2d6b92e0c
kidus-t-w <kidusk69@gmail.com>
2024-05-23 20:58:55 +0300

    Added create_session method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -13,6 +13,9 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """
+        Creates session
+        """
         if user_id is None or type(user_id) != str:
             return None
         self.session_id = uuid.uuid4()
