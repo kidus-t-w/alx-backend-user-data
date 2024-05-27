a94812d839094350effb84427761e19ba3fb02db
kidus-t-w <kidusk69@gmail.com>
2024-05-27 16:35:40 +0300

    Modified before_request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,8 +38,7 @@ def before_request():
                      '/api/v1/unauthorized/',
                      '/api/v1/forbidden/',
                      '/api/v1/auth_session/login/']
-    if auth.authorization_header(request) is None \
-        and auth.session_cookie(request) is None:
+    if auth.authorization_header(request) and auth.session_cookie(request):
         abort(401)
     if auth and auth.require_auth(request.path, excluded_path):
         if auth.authorization_header(request) is None:
