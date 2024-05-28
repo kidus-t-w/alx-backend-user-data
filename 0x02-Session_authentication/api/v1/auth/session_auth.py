942d6f958f04599fe282f89d762f15291ebeae4c
kidus-t-w <kidusk69@gmail.com>
2024-05-28 11:32:34 +0300

    Modified /auth_sessio/login route

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Session Authentication Views"""
+""" Module of Session authentication views
+"""
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
@@ -7,25 +8,41 @@ from os import getenv
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def session_login() -> str:
-    """
-    POST /api/v1/auth_session/login
+def login():
+    """ POST /auth_session/login
+    Return
+        - Logged in user
     """
     email = request.form.get('email')
-    password = request.form.get('password')
-    if email is None or email == '':
+
+    if not email:
         return jsonify({"error": "email missing"}), 400
-    if password is None or password == '':
+
+    password = request.form.get('password')
+
+    if not password:
         return jsonify({"error": "password missing"}), 400
-    user = User.search({'email': email})
-    if user is None or len(user) == 0:
+
+    try:
+        found_users = User.search({'email': email})
+    except Exception:
         return jsonify({"error": "no user found for this email"}), 404
-    user = user[0]
-    if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
+
+    if not found_users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    for user in found_users:
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+
     from api.v1.app import auth
+
+    user = found_users[0]
     session_id = auth.create_session(user.id)
+
+    SESSION_NAME = getenv("SESSION_NAME")
+
     response = jsonify(user.to_json())
-    SESSION_ID = getenv('SESSION_NAME')
-    response.set_cookie(SESSION_ID, session_id)
+    response.set_cookie(SESSION_NAME, session_id)
+
     return response
