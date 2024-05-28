60b807b6e207563e87ea2922029fd4cf5131f345
kidus-t-w <kidusk69@gmail.com>
2024-05-28 11:30:07 +0300

    Modified

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,53 +1,31 @@
 #!/usr/bin/env python3
-"""
-Session Authentication
-"""
-from typing import TypeVar
-from api.v1.auth.auth import Auth
-import uuid
+"""Session Authentication Views"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
 from models.user import User
+from os import getenv
 
 
-class SessionAuth(Auth):
+@app_views.route('/api/v1/auth_session/login', methods=['POST'], strict_slashes=False)
+def session_login() -> str:
     """
-    Session Authentication
+    POST /api/v1/auth_session/login
     """
-
-    user_id_by_session_id = {}
-
-    def create_session(self, user_id: str = None) -> str:
-        """
-        Creates session
-        """
-        if user_id is None or not isinstance(user_id, str):
-            return None
-        self.session_id = str(uuid.uuid4())
-        self.user_id_by_session_id[self.session_id] = user_id
-        return self.session_id
-
-    def user_id_for_session_id(self, session_id: str = None) -> str:
-        """
-        Retrieves the user ID associated with the given session ID.
-
-        Parameters:
-            session_id (str, optional): The session ID to retrieve the
-            user ID for. Defaults to None.
-
-        Returns:
-            str: The user ID associated with the session ID,
-            or None if the session ID is invalid or not found.
-        """
-        if session_id is None or not isinstance(session_id, str):
-            return None
-
-        return self.user_id_by_session_id.get(session_id)
-
-    def current_user(self, request=None) -> TypeVar('User'):
-        """
-        Returns user instance using cookie
-        """
-        session_id = self.session_cookie(request)
-        if session_id is None:
-            return None
-        user_id = self.user_id_for_session_id(session_id)
-        return User.get(user_id)
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or email == '':
+        return jsonify({"error": "email missing"}), 400
+    if password is None or password == '':
+        return jsonify({"error": "password missing"}), 400
+    user = User.search({'email': email})
+    if user is None or len(user) == 0:
+        return jsonify({"error": "no user found for this email"}), 404
+    user = user[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    SESSION_ID = getenv('SESSION_NAME')
+    response.set_cookie(SESSION_ID, session_id)
+    return response
