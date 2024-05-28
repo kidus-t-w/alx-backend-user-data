38b4eabf0f4151b59a9b7fda00175fef524b70c1
kidus-t-w <kidusk69@gmail.com>
2024-05-28 11:42:31 +0300

    Modified /auth_sessio/login route

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-""" Module of Users views
-"""
+""" Module of Users views """
 import os
 from flask import jsonify, request
 from api.v1.views import app_views
 from models.user import User
-
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def auth_session():
@@ -16,19 +14,30 @@ def auth_session():
     """
     email = request.form.get('email')
     password = request.form.get('password')
-    if email is None or email == '':
+    
+    if not email:
         return jsonify({"error": "email missing"}), 400
-    if password is None or password == '':
+    
+    if not password:
         return jsonify({"error": "password missing"}), 400
+    
     users = User.search({"email": email})
-    if not users or users == []:
+    if not users:
         return jsonify({"error": "no user found for this email"}), 404
-    for user in users:
-        if user.is_valid_password(password):
-            from api.v1.app import auth
-            session_id = auth.create_session(user.id)
-            resp = jsonify(user.to_json())
-            session_name = os.getenv('SESSION_NAME')
-            resp.set_cookie(session_name, session_id)
-            return resp
-    return jsonify({"error": "wrong password"}), 401
+    
+    user = users[0]  # Assuming email is unique and only one user is found
+
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    
+    resp = jsonify(user.to_json())
+    session_name = os.getenv('SESSION_NAME', '_my_session_id')
+    resp.set_cookie(session_name, session_id)
+    
+    return resp
+
+# Ensure to add the view in api/v1/views/__init__.py
+# from api.v1.views.session_auth import *
