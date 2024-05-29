e2cac965d1fcb33681c136cc67bb2c53a63afb73
kidus-t-w <kidusk69@gmail.com>
2024-05-29 11:36:34 +0300

    Modified add_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,17 +31,11 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database and return the User object.
-
-        Args:
-            email (str): The email of the new user.
-            hashed_password (str): The hashed password of the new user.
-
-        Returns:
-            User: The created User object.
+        """ Adds user to database
+        Return: User Object
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
 
-        return new_user
+        return user
