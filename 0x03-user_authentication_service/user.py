5a78c8468e882aef0ac05c4c961effbd5a30f7ac
kidus-t-w <kidusk69@gmail.com>
2024-05-29 10:56:21 +0300

    Added docstirng for User class

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+User model
+"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
@@ -6,6 +9,9 @@ Base = declarative_base()
 
 
 class User(Base):
+    """
+    User model
+    """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
