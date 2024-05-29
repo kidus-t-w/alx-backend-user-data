b0dd7e9faad0e7ccea928e2555c42ef5e84d0cb1
kidus-t-w <kidusk69@gmail.com>
2024-05-29 11:22:10 +0300

    Added add_user method

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,48 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email, hashed_password):
+        # Get the session
+        db = self._session
+
+        # Create a new user
+        new_user = User(email=email, hashed_password=hashed_password)
+
+        # Add the new user to the session
+        db.add(new_user)
+
+        # Commit the session to save the new user
+        db.commit()
+
+        # Close the session
+        db.close()
+
+        return new_user
