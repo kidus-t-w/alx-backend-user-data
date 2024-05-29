1224b21c604c24aeafca891d957f29ac648880d3
kidus-t-w <kidusk69@gmail.com>
2024-05-29 10:55:38 +0300

    Modified User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
+Base = declarative_base()
 
-class User():
+
+class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
-    
-    def __repr__(self):
-       return "<User(name='%s', fullname='%s', nickname='%s')>" % (
-                            self.name, self.fullname, self.nickname)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
