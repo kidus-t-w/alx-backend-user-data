845fd4122b2cef54c37840e058956d013ca6fbb6
kidus-t-w <kidusk69@gmail.com>
2024-05-30 14:31:08 +0300

    Added _hash_password method

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Authentication Module """
+
+import bcrypt
+from db import DB
+# from sqlalchemy.orm.exc import NoResultFound
+# from typing import Union
+# from user import User
+# from uuid import uuid4
+
+
+def _hash_password(password: str) -> str:
+    """ Returns a salted hash of the passed password """
+    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hashed
