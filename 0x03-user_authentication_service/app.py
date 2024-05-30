1097569392f78d8de1121f1bb5d78b455b116bd8
kidus-t-w <kidusk69@gmail.com>
2024-05-30 14:41:21 +0300

    Added /users route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -11,6 +11,23 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_world() -> str:
-    """ Base route for authentication service API """
+    """ Base route"""
     msg = {"message": "Bienvenue"}
+    return jsonify(msg)
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """Registers a new user if it doesn't exist"""
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    msg = {"email": email, "message": "user created"}
     return jsonify(msg)
