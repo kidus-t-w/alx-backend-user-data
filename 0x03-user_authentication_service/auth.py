34aa4d7ccaa08f97ea5816974bc7288f9d042f82
kidus-t-w <kidusk69@gmail.com>
2024-05-30 14:51:38 +0300

    Added get_user_form_session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -58,3 +58,17 @@ class Auth:
             return True
 
         return False
+
+    def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
+        """It takes a single session_id string argument
+        Returns a string
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+
+        return user
