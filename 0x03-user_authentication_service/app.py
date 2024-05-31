e2d9b40dc06e8d96e9adc0f5bce48886b80b1070
kidus-t-w <kidusk69@gmail.com>
2024-05-31 13:05:43 +0300

    Created flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
-"""
-Flask App
-"""
-from flask import Flask, jsonify, request, abort
+"""API Routes for Authentication Service"""
 from auth import Auth
+from flask import (Flask,
+                   jsonify,
+                   request,
+                   abort)
 
 app = Flask(__name__)
 AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
-def hello_world():
-    """
-    Base route
-    """
+def hello_world() -> str:
+    """ Base route for authentication service API """
     msg = {"message": "Bienvenue"}
     return jsonify(msg)
 
