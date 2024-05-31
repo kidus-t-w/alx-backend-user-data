1d5bb5c9e053821eeb19f814af200d12df3c9963
kidus-t-w <kidusk69@gmail.com>
2024-05-31 10:27:09 +0300

    Modified find_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -58,7 +58,7 @@ class DB:
             raise
         return new_user
 
-    def find_user_by(self, **kwargs: Dict[str, str]) -> User:
+    def find_user(self, **kwargs: Dict[str, str]) -> User:
         """Find a user by specified attributes.
 
         Raises:
@@ -68,19 +68,16 @@ class DB:
         Returns:
             User: First row found in the `users` table.
         """
-        fields, values = [], []
-        for key, value in kwargs.items():
-            if hasattr(User, key):
-                fields.append(getattr(User, key))
-                values.append(value)
-            else:
-                raise InvalidRequestError()
-        result = self._session.query(User).filter(
-            tuple_(*fields).in_([tuple(values)])
-        ).first()
-        if result is None:
-            raise NoResultFound()
-        return result
+        column = User.__table__.columns
+        if kwargs is None:
+            raise InvalidRequestError
+        for key in kwargs:
+            if key not in column:
+                raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).one()
+        if user is None:
+            raise NoResultFound
+        return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """Updates a user based on a given id.
