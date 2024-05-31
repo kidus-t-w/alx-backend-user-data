2c8eab84f8cf92fd6e04f4e2a430dcbe082d27f2
kidus-t-w <kidusk69@gmail.com>
2024-05-31 11:58:28 +0300

    Added /users route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,11 @@
 """
 Flask App
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -14,6 +16,16 @@ def home():
     """
     return jsonify({"message": "Bienvenue"})
 
+@app.route('/users', methods=['POST'])
+def users():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    user = AUTH.register_user(email, password)
+    if user:
+        return jsonify({"email": email, "message":"user created"})
+    return jsonify({"message": "email already registered"}), 400
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
