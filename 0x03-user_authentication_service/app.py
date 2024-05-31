97fa04f06555d3a8a804eada1adee8bada44b76c
kidus-t-w <kidusk69@gmail.com>
2024-05-31 18:22:26 +0300

    Added GET  /profile route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -71,7 +71,7 @@ def profile():
     """
     Get user profile
     """
-    session_id = request.cookies.get(session_id)
+    session_id = request.cookies.get("session_id")
     if session_id is None:
         abort(403)
     user = AUTH.get_user_from_session_id(session_id)
