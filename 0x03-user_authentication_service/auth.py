efc7b6b364674dc2c997416d01c52a6d969a8c5a
kidus-t-w <kidusk69@gmail.com>
2024-05-31 17:24:41 +0300

    Added get_user_from_session_id method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,3 +72,12 @@ class Auth:
             return None
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str):
+        """
+        Returns User object with session_id
+        """
+        user = self._db.find_user_by(session_id=session_id)
+        if user:
+            return user
+        return None
